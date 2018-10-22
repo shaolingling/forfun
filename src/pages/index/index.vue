f530011641aed12eac5eaacd5ab9b2a215b6686d
Lingling Shao <shao.lingling@opg.cn>
2018-10-22 17:24:21 +0800

    新建项目

---
+++ src/pages/index/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+     <van-tabs v-model="active">
+          <van-tab title="标签 1">内容 1</van-tab>
+          <van-tab title="标签 2">内容 2</van-tab>
+          <van-tab title="标签 3">内容 3</van-tab>
+          <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+    <!-- <div class="userinfo" @click="bindViewTap">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+      <div class="userinfo-nickname">
+        <card :text="userInfo.nickName"></card>
+      </div>
+    </div>
+
+    <div class="usermotto">
+      <div class="user-motto">
+        <card :text="motto"></card>
+      </div>
+    </div>
+
+    <form class="form-container">
+      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
+      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
+    </form>
+    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a> -->
+  </div>
+</template>
+
+<script>
+// import card from '@/components/card'
+import { Tab, Tabs } from "vant";
+Vue.use(Tab).use(Tabs);
+
+export default {
+  data() {
+    return {
+      active: 2
+    };
+  },
+
+  methods: {
+    bindViewTap() {
+      const url = "../logs/main";
+      wx.navigateTo({ url });
+    },
+    getUserInfo() {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: res => {
+              this.userInfo = res.userInfo;
+            }
+          });
+        }
+      });
+    },
+    clickHandle(msg, ev) {
+      console.log("clickHandle:", msg, ev);
+    }
+  },
+
+  created() {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo();
+  }
+};
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+
+.counter {
+  display: inline-block;
+  margin: 10px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
